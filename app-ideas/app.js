650167f86b92d6734f03bb083657e83a07eea23b
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-05-17 16:49:45 -0400

    Add files via upload

--- app-ideas/app.js
+++ app-ideas/app.js
@@ -1,5 +1,9 @@
 function generate() {
-        let genres = ["Fitness", "Social Media", "Education", "Video Player", "Shopping", "Other"];
+        let genres = ["Fitness", "SocialMedia", "Education", "VideoPlayer", "Shopping", "Other"];
         var ranG = Math.floor(Math.random() * genres.length);
         document.getElementById("gen").innerHTML = genres[ranG]; 
+}
+function appnews() {
+        var el = document.getElementById("gen").textContent;
+        window.open("https://www.businessofapps.com/news/" + el);
 }
